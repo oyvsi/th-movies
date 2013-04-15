3e4a6eb0cbe901a79f21b6fb17a9edcfc4a59b58
newsflash@gmail.com <newsflash@gmail.com@9c822035-d77f-97b9-37ab-375e2658a918>
2013-04-15 19:05:14 +0000

    GL, bitches

--- trunk/app/webroot/js/thMovies.jquery.js
+++ trunk/app/webroot/js/thMovies.jquery.js
@@ -1,8 +1,14 @@
+function sendRating(test) {
+	console.log("This is where we get our ajax on. Vars in: " + test);
+}
+
 $(document).ready(function() {
 	$('#star').raty({
 		number: 12,	
 		hints: ['Worst fucking movie, like, evah!', 'Worst', 'Terrible', 'Really bad', 'Bad', 'OK', 'Good', 'Really good', 'Great', 'Awesome', 'I want to have this movies babies', 'Lifechaning, out of body experience'],
 		target: '#rating-hint',
+		targetKeep: true,
 		width: false,
+		click: sendRating,
 	});
 });
