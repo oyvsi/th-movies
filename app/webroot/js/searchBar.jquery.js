f7c40313657dd376c59179212baba364e6efbf02
bundy.strand <bundy.strand@gmail.com>
2013-04-16 18:53:16 +0000

    Searchbox listing 5 items, then listing 5 more when down arrowing to the end

--- app/webroot/js/searchBar.jquery.js
+++ app/webroot/js/searchBar.jquery.js
@@ -10,6 +10,13 @@ $(document).ready(function() {
       $.each(data.results, function(index, movie) {
 				var output = '<li class="search_item" id="' + movie.id + '"><span id="title">' + movie.original_title + ' (' + movie.release_date + ')</span><span id="thumb"><img src="http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w92' + movie.poster_path + '" /></thumb></li>';
 				$("#movies").append(output);
+				$('#movies li:gt(4)').hide();
+				if(index == 4) {
+					$("#movies").append('<li class="search_item" id="moremovies">More movies! (Down Arrow here m8y) </li>');
+				}
+				if(index == 9) {
+					return false;
+				}
 		});
 	}
 
@@ -27,12 +34,18 @@ $(document).ready(function() {
 		var $prev, $next, $current = $("#movies li.selected");		
 		if(inputRegEx.test(String.fromCharCode(event.which)) || ($.inArray(event.keyCode, allowedKeys>-1))) {
 			if(event.keyCode === 13) {
-				if($current.length) {
+				 if ($current.length) {
 					document.location = '/www-tek/th-movies/movies/' + $current.attr('id');
 					//console.log($current.attr('id'));
-				}
+					} 
 			} else if (event.keyCode === 40) {
 				if(!$current.length) $("#movies li:first").addClass("selected");
+				if ($current.attr('id') == 'moremovies') {
+						$next = $('li[id*=moremovies]').next();
+						$('#movies li:gt(4)').show();
+						$('li[id*=moremovies]').remove();
+						$next.addClass('selected');
+				}
 				$next = $current.next("li");
 				if ($next.length) {
 					$current.removeClass("selected");
