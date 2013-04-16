709192a9d11ac20b558a962ce341ba9eaedb47c4
newsflash <newsflash@gmail.com>
2013-04-16 18:43:57 +0000

    Rating movies now works

--- app/Controller/MoviesController.php
+++ app/Controller/MoviesController.php
@@ -3,6 +3,8 @@
 class MoviesController extends AppController {
 	private $apikey = 'a8049ffed9e54f709cc42647f7a42722';
 	private $rtBase = 'http://api.themoviedb.org/3/movie/';
+	var $components = array('RequestHandler');
+
 
 	private function get_json($url) {
 		$ch = curl_init();
@@ -22,14 +24,27 @@ class MoviesController extends AppController {
 	}
 
 	public function view() {
+		$user = $this->Auth->user();
 		$id = $this->request->params['id'];
+		if($user) {
+			$this->loadModel('Rating');
+			$rating = $this->Rating->find('first', array('conditions' => array('user_id' => $user['id'], 'movie_id' => $id), 'fields' => array('rating')));
+			if($rating)
+				$this->set('rating', $rating['Rating']['rating']);
+		}
+
 		$movie = $this->get_json($this->rtBase . $id . '?api_key=' . $this->apikey);
 		$this->set('movie', json_decode($movie));
 		$this->render('index');
-
 	}
 
-	public function rate($movie = null) {
-	
+	public function rate() {
+		$this->loadModel('Rating');
+		if($this->request->is('ajax')) {
+			$this->layout = 'ajax';
+			$user = $this->Auth->user();
+			$data = array('user_id' => $user['id'], 'movie_id' => $this->data['movie'], 'rating' => $this->data['rating'], 'timestamp' => time());
+			$this->Rating->save($data);
+		}	
 	}
 }
