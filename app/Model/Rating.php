36ad4f71cda0c139d01e0c278caa9179f4d9212b
newsflash <newsflash@gmail.com>
2013-04-16 18:44:31 +0000

    Forgot our pretty model

---
+++ app/Model/Rating.php
@@ -0,0 +1,27 @@
+<?php
+class Rating extends AppModel {
+    
+ /*   public $validate = array(
+      'movie_id' => array(
+          array(
+            'rule' => 'notEmpty',
+            'message' => 'Movie cannot be empty'
+          ),
+          array(
+            'rule' => 'isUnique',
+            'message' => 'This username is already taken'
+          )          
+      ),
+      'rating' => array(
+          array(
+            'rule' => 'notEmpty',
+            'message' => 'Password cannot be empty'
+          ),
+          array(
+            'rule' => array('minLength', 4),
+            'message' => 'Must be at least 4 chars' 
+          ),
+      )
+	);*/
+    
+}
