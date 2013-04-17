fd40741f52dd4cb82d5647f26f4ba6b00459afee
newsflash <newsflash@gmail.com>
2013-04-17 13:24:54 +0000

    Created global var baseURL

--- app/webroot/js/thMovies.jquery.js
+++ app/webroot/js/thMovies.jquery.js
@@ -4,7 +4,7 @@ function sendRating(rating) {
 
 	$.ajax({
 		type: 'POST',
-		url: 'http://bmore.teamhenkars.com/www-tek/th-movies/movies/rate',
+		url: baseURL + 'movies/rate',
 		data: { movie_id: id, rating: rating },
 	}).done(function(result) {
 		console.log('good ajax gave us:' + result);
@@ -14,6 +14,7 @@ function sendRating(rating) {
 
 $(document).ready(function() {
 	$('#star').raty({
+		path: baseURL + '/js/img/',
 		number: 12,	
 		hints: ['Worst fucking movie, like, evah!', 'Worst', 'Terrible', 'Really bad', 'Bad', 'OK', 'Good', 'Really good', 'Great', 'Awesome', 'I want to have this movies babies', 'Lifechaning, out of body experience'],
 		target: '#rating-hint',
