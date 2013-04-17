3388abb7f25c0108d0c6d81931c6255fd5d9aa4d
newsflash <newsflash@gmail.com>
2013-04-17 18:45:28 +0000

    Once again, forgot a model

---
+++ app/Model/Movie.php
@@ -0,0 +1,30 @@
+<?php
+class Movie extends AppModel {
+	public $primaryKey = 'movie_id';    
+	public $hasMany = array('Rating' => array('className' => 'Rating', 'foreignKey' => 'movie_rating_id'));
+///	public $hasOne = 'Movie';
+
+ /*   public $validate = array(
+      'movie_id' => array(
+          array(
+            'rule' => 'notEmpty',
+            'message' => 'Movie cannot be empty'
+          ),
+          array(
+            'rule' => 'isUnique',
+            'message' => 'This username is already taken'
+          )          
+      ),
+      'rating' => array(
+          array(
+            'rule' => 'notEmpty',
+            'message' => 'Password cannot be empty'
+          ),
+          array(
+            'rule' => array('minLength', 4),
+            'message' => 'Must be at least 4 chars' 
+          ),
+      )
+	);*/
+    
+}
