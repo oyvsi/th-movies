97e813f52a662074c66926e9281b0c04f42e29ea
newsflash <newsflash@gmail.com>
2013-04-17 09:29:05 +0000

    Fix for updating rating

--- app/Controller/MoviesController.php
+++ app/Controller/MoviesController.php
@@ -1,10 +1,56 @@
 <?php
 
 class MoviesController extends AppController {
+	private $user;
 	private $apikey = 'a8049ffed9e54f709cc42647f7a42722';
 	private $rtBase = 'http://api.themoviedb.org/3/movie/';
 	var $components = array('RequestHandler');
 
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->loadModel('Rating');
+		$this->user = $this->Auth->user();
+
+		if(isset($this->request->params['id']))
+			$this->movie_id = $this->request->params['id'];
+		elseif(isset($this->data['movie_id']))
+			$this->movie_id = $this->data['movie_id'];
+		else
+			$this->setAction('movieList');
+
+		if(isset($this->movie_id) && $this->user) {
+			$this->Rating->data = $this->Rating->find('first', array('conditions' => array('user_id' => $this->user['id'], 'movie_id' => $this->movie_id)));
+			if(!($this->Rating->data)) {
+				$this->Rating->create();
+				$this->Rating->set(array('user_id' => $this->user['id'], 'movie_id' => $this->movie_id));
+			} else
+				$this->Rating->id = $this->Rating->data['Rating']['movie_rating_id'];
+		}	
+	}
+
+	public function movieList() {
+		echo "Cool list of movies or something";
+	}
+
+	public function view() {
+		if($this->user && isset($this->Rating->data['Rating']['rating'])) {
+			$this->set('rating', $this->Rating->data['Rating']['rating']);
+		}
+
+		$movie = $this->get_json($this->rtBase . $this->movie_id . '?api_key=' . $this->apikey);
+		$this->set('movie', json_decode($movie));
+		$this->render('index');
+	}
+
+	public function rate() {
+		if($this->request->is('ajax')) {
+			$this->layout = 'ajax';
+			if($this->Rating->id)
+				$this->Rating->saveField('rating', $this->data['rating']);
+			else
+				$this->Rating->save($this->data);
+		}	
+	}
 
 	private function get_json($url) {
 		$ch = curl_init();
@@ -16,35 +62,5 @@ class MoviesController extends AppController {
 		curl_close($ch);
 
 		return $response;
-	}
-
-	public function index($movie = null) {
-		$this->set('movie', $this->paginate());
-		$this->set('query', $movie);
-	}
-
-	public function view() {
-		$user = $this->Auth->user();
-		$id = $this->request->params['id'];
-		if($user) {
-			$this->loadModel('Rating');
-			$rating = $this->Rating->find('first', array('conditions' => array('user_id' => $user['id'], 'movie_id' => $id), 'fields' => array('rating')));
-			if($rating)
-				$this->set('rating', $rating['Rating']['rating']);
-		}
-
-		$movie = $this->get_json($this->rtBase . $id . '?api_key=' . $this->apikey);
-		$this->set('movie', json_decode($movie));
-		$this->render('index');
-	}
-
-	public function rate() {
-		$this->loadModel('Rating');
-		if($this->request->is('ajax')) {
-			$this->layout = 'ajax';
-			$user = $this->Auth->user();
-			$data = array('user_id' => $user['id'], 'movie_id' => $this->data['movie'], 'rating' => $this->data['rating'], 'timestamp' => time());
-			$this->Rating->save($data);
-		}	
 	}
 }
